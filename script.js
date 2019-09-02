eb62eead419b95033ad58868c466385d6374992f
DevJxn <54246093+DevJxn@users.noreply.github.com>
2019-09-02 16:47:58 +0200

    02/09/19 - 1

--- script.js
+++ script.js
@@ -65,7 +65,8 @@ function getBundleName(pBundleName,pTriggerElement){
                       "images/forestScreen.jpg",
                       "images/torontoScreen.jpg",
                       "images/circlesScreen.jpg",
-                      "images/triangleScreen.jpg"];
+                      "images/triangleScreen.jpg",
+                      "images/cubeScreen.jpg"];
 
   }else if(pBundleName === document.getElementById("simpleStatsBundle")){
     var imageLinks = ["images/simpleBundleScreen.jpg",   //change to simple screen
