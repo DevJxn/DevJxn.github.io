01116b701074ed0de7a1e6d94690fbb1c7e508b8
DevJxn <54246093+DevJxn@users.noreply.github.com>
2019-09-03 20:22:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
                 document.getElementById("checkbox").checked = false;
             }
 
-       } // end if
+       }
 
     }); // end link function (event)
 
